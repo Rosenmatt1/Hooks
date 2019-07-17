d84885cb74f9e5627799c08ce816b0fb3b1945ce
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-16 22:57:10 -0600

    Added styling for todo list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import './App.css'
+
+function Todo({ todo, index }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  )
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
+  return  (
+    <form onSumbit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
 
 function App() {
-  
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false,
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false,
+    },
+    {
+      text: "Build cool todo app",
+      isCompleted: false,
+    }
+  ]);
+
+  return (
+    <div className="App">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App;
